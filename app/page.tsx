7de4f3d70553a277915ddb104196f3028967c169
SJLuisGabriel <19031333@itcelaya.edu.mx>
2024-05-30 14:23:15 -0600

    Actualizacion de rutas

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
+// app/page.tsx
+"use client";
+
 import { useEffect } from "react";
-import { useRouter } from "next/router";
-import Image from "next/image";
+import { useRouter } from "next/navigation"; // Importa next/navigation para Componentes del Servidor
 
 export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    // Redirige a la ruta deseada, por ejemplo, '/otra-ruta'
-    router.push("/order/cafe");
+    // Código que utiliza useEffect
+    // Ejemplo: redirigir a la ruta /order/hamburguesa
+    router.push("/order/hamburguesa");
   }, [router]);
 
   return <h1>Hola mundo</h1>;
